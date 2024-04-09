2951bce18e129a8160d42af3a53c73a92725e18a
Doth-J <theodjoan@gmail.com>
2024-04-09 03:04:51 +0300

    minor

--- page/vite.config.ts
+++ page/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/dockerforge/",
   server: {
     port: 4000,
   },
